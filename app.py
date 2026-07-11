062b298247c1b8812921354eb1111f389ff5322c
Waqar Younis <waqaryounisc1@gmail.com>
2026-07-11 14:25:00 +0530

    Added multiple city search

--- app.py
+++ app.py
@@ -4,20 +4,28 @@ from weather import get_weather
 
 show_title()
 
-city = input("Enter your city: ")
+while True:
 
-weather = get_weather(city)
+    city = input("\nEnter your city (or type 'exit'): ")
 
-line()
+    if city.lower() == "exit":
+        print("\n👋 Thank you for using Weather Application!")
+        break
 
-if weather:
+    weather = get_weather(city)
 
-    print(f"📍 City        : {weather['city']}, {weather['country']}")
-    print(f"🌡 Temperature : {weather['temperature']} °C")
-    print(f"🥵 Feels Like : {weather['feels_like']} °C")
-    print(f"💧 Humidity    : {weather['humidity']}%")
-    print(f"☁ Condition   : {weather['condition'].title()}")
-    print(f"🌬 Wind Speed  : {weather['wind']} m/s")
+    line()
 
-else:
-    print("❌ City not found.")
+    if weather:
+
+        print(f"📍 City        : {weather['city']}, {weather['country']}")
+        print(f"🌡 Temperature : {weather['temperature']} °C")
+        print(f"🥵 Feels Like  : {weather['feels_like']} °C")
+        print(f"💧 Humidity    : {weather['humidity']}%")
+        print(f"☁ Condition   : {weather['condition'].title()}")
+        print(f"🌬 Wind Speed  : {weather['wind']} m/s")
+
+    else:
+        print("❌ City not found.")
+
+    line()
