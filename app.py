81b020fa6f2dddcd7d074bc25d10f0758990f310
Waqar Younis <waqaryounisc1@gmail.com>
2026-07-11 13:44:22 +0530

    Display formatted weather information

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
+from weather import get_weather
+
 print("=" * 40)
-print("      🌦️ Weather Application")
+print("🌦️ Weather Application")
 print("=" * 40)
 
 city = input("Enter your city: ")
 
-print(f"\nSearching weather for: {city}")
+weather_data = get_weather(city)
+
+print(weather_data)
--- weather.py
+++ weather.py
@@ -1,9 +1,15 @@
 import requests
+from config import API_KEY
 
-url = "https://api.github.com"
 
-response = requests.get(url)
+def get_weather(city):
+    url = (
+        f"https://api.openweathermap.org/data/2.5/weather"
+        f"?q={city}"
+        f"&appid={API_KEY}"
+        f"&units=metric"
+    )
 
-print("Status Code:", response.status_code)
-print()
-print(response.text)
+    response = requests.get(url)
+
+    return response.json()
